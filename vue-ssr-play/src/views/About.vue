1121b4aa6a25e06bb05a210ff9a6827559826edb
Eddie Zhang <ezhang7423@student.palomar.edu>
2019-11-27 00:36:10 -0800

    add vue ssr play

---
+++ vue-ssr-play/src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home-view">
+    <div class="container">
+      <h1>About</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'about-view',
+  computed: {
+  },
+  beforeMount() {
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue-ssr-play/src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home-view">
+    <div class="content-top">
+      <div class="container">
+        <h1>vue-ssr-boilerplate</h1>
+        <p>This is a stripped down version of the
+          <a href="https://github.com/vuejs/vue-hackernews-2.0">vue-hackernews-2.0</a>
+          demo featuring Vue 2.0, vue-router, and vuex, with server-side rendering.
+        </p>
+        <div class="panel panel-default">
+          <div class="panel-heading">Prefetched items</div>
+          <div class="panel-body">
+            <ul>
+              <li v-for="item in items">
+                {{ item.title }}: {{ item.description }}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+function fetchItems(store) {
+  return store.dispatch('LOAD_ACTIVE_ITEMS');
+}
+
+export default {
+  name: 'home-view',
+  computed: {
+    items() {
+      return this.$store.getters.activeItems;
+    },
+  },
+  preFetch: fetchItems,
+  beforeMount() {
+    fetchItems(this.$store);
+  },
+};
+</script>
+
+<style>
+</style>
