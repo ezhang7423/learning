49a897b8b7f9694a16c059cddd14e7acf0d1ab2f
Eddie Zhang <ezhang7423@student.palomar.edu>
2019-12-04 22:10:15 -0800

    addNodeBack

--- cpp/recursive-cpp-helper-functions/ll-helpers.cpp
+++ cpp/recursive-cpp-helper-functions/ll-helpers.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 using namespace std;
 
-struct node
+struct Node
 {
-  node *next;
-  long long value;
+  Node *next;
+  long long data;
 };
 
-node *addNodeFront(node *head, long long value)
+Node *addNodeFront(Node *head, long long value)
 {
-  node *p = new node{head, value};
+  Node *p = new Node{head, value};
   head = p;
   return head;
+}
+
+Node *addNodeBack(Node *head, long long value, Node *ogHead)
+{
+  if (!head->next)
+  {
+    Node *p = new Node;
+    p->next = nullptr;
+    p->data = value;
+    head->next = p;
+    return ogHead;
+  }
+  return addNodeBack(head->next, value, ogHead);
 }
 
 long long pow(int base, long long power)
@@ -21,38 +34,60 @@ long long pow(int base, long long power)
   return pow(base, power - 1) * base;
 }
 
-node *_addManyNodes(node *head, long long timesToCall)
+Node *_addManyNodes(Node *head, long long timesToCall)
 { //add function to act as operator
-  if (timesToCall <= 0)
+  if (timesToCall == 0)
   {
     return head;
   }
   head = addNodeFront(head, pow(2, timesToCall));
-  cout << timesToCall << " " << head->next->value << endl;
-  _addManyNodes(head, timesToCall - 1);
-  return head;
+  // cout << timesToCall << " " << head -> next -> data << endl;
+  return _addManyNodes(head, timesToCall - 1);
 }
 
-// void _addManyNodes(node*& head, long long timesToCall){ //add function to act as operator
-//   if (timesToCall <= 0){
-//     return;
-//   }
-//   _addManyNodes(head, timesToCall-1);
-//   head = addNodeFront(head, timesToCall);
-// }
-void printLl(node *head)
+void deleteVNodesHelper(Node *head, int value, Node *lastHead)
 {
-  if (head == nullptr)
+  if (head->data == value)
+  {
+    lastHead->next = head->next;
+  }
+  if (!head->next)
   {
     return;
   }
-  cout << head->value << endl;
+  if (head->data == value)
+    deleteVNodesHelper(head->next, value, lastHead);
+  else
+    deleteVNodesHelper(head->next, value, lastHead->next);
+}
+
+Node *deleteVNodes(Node *head, int value)
+{
+  if (head == nullptr)
+  {
+    return head;
+  }
+  deleteVNodesHelper(head->next, value, head);
+  //catch if value == first item
+  if (head->data == value)
+  {
+    return head->next;
+  }
+  return head;
+}
+
+void printLl(Node *head)
+{
+  if (head == nullptr)
+  {
+    cout << "NULL" return;
+  }
+  cout << head->data << "->";
   printLl(head->next);
 }
-int main()
-{
-  node *head = 0;
-  // _addManyNodes(head, 15);
-  head = _addManyNodes(head, 1);
-  printLl(head);
-}
+// int main(){
+//   Node* head = 0;
+//   // _addManyNodes(head, 15);
+//   head = _addManyNodes(head, 1);
+//   printLl(head);
+// }
