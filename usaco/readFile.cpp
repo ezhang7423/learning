9c46deda0c4a6fb55ba7de7f1aa1940ba26bbde4
ezhang7423 <ezhang7423@student.palomar.edu>
2019-12-08 10:00:59 -0800

    utility functions

--- usaco/readFile.cpp
+++ usaco/readFile.cpp
@@ -6,14 +6,29 @@ TASK: test
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <vector>
 using namespace std;
 
+
+
+void printArray(string arr[], int len){
+    for (int i = 0; i < len; i++) cout << arr[i];
+}
+
+void printVector(vector<string> &vect){
+    for (int i = 0; i < vect.size(); i++) cout << vect[i] << endl;
+}
+
+
 int main() {
-    ofstream out ("test.out");
-    ifstream in ("test.in");
-    int a, b;
-    in >> a >> b;
-    out << a+b << "\n";
+    ifstream in ("sample.txt");
+    string str, file;
+    vector<string> data;
+    while (getline(in, str)){
+        data.push_back(str);
+    }
+    printVector(data);
     return 0;
 }
+
+
