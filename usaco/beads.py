170a0c293e7d6d8c7aa986ef011bd65a650c5031
Eddie Zhang <54998055+ezhang7423@users.noreply.github.com>
2019-12-08 19:57:42 -0800

    working?

--- usaco/beads.py
+++ usaco/beads.py
@@ -3,16 +3,27 @@ data = open('beads.in', 'r')
 index, s = data.read().split('\n')
 index = int(index)
 
+
+def printCurrentIndex(index):
+  print(coolString)
+  print(" "*index + '^')
+
+
 def findValues(up, index):
     value = 1
     iteration = 0
+    if (up == -1):
+      initValue = coolString[index-1]
+      index -= 1
     initValue = coolString[index]
     tIndex = index
     while (initValue == 'w'):
         initValue = coolString[tIndex + up]
         tIndex += up
-    while (coolString[index + up] == initValue or coolString[index + up] == 'w'
-           and iteration < math.floor(index / 2)):
+    while (coolString[index] == initValue or coolString[index] == 'w'
+           and iteration < math.floor(index)):
+        printCurrentIndex(index)
+        print(initValue, value)
         value += 1
         index += up
         iteration += 1
@@ -26,9 +37,11 @@ initValue = coolString[index]
 for ite in range(index, 2 * index):
     lVal = findValues(1, ite)
     rVal = findValues(-1, ite)
-    cVal = lVal + rVal
+    cVal = lVal + rVal -2
     if (cVal > maxe):
         maxe = cVal
+    print('total value: ', cVal)
+    print('-------------------')
 
 if (maxe > index):
   print(index)
