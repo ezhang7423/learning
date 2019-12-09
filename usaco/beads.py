593285156999f1ba40cad6a6613020b48904deb7
ezhang7423 <ezhang7423@student.palomar.edu>
2019-12-08 22:53:23 -0800

    finished

--- usaco/beads.py
+++ usaco/beads.py
@@ -1,12 +1,22 @@
 '''
-TASK: futzipe1
+TASK: beads
 LANG: PYTHON3
-PROB: beads
+ID: futzipe1
 '''
 import math
 data = open('beads.in', 'r')
-index, s = data.read().split('\n')
+# index, s = 
+data = data.read().split('\n')
+index = data[0]
+s = data[1]
+s = s.strip()
 index = int(index)
+
+
+
+def printCurrentIndex(index):
+  print(coolString)
+  print(" "*index + '^')
 
 def findValues(up, index):
     value = 1
@@ -16,33 +26,33 @@ def findValues(up, index):
       index -= 1
     initValue = coolString[index]
     tIndex = index
-    while (initValue == 'w'):
+    while ( ((tIndex < len(coolString) - 1) and tIndex > 0) and initValue == 'w'):
         initValue = coolString[tIndex + up]
         tIndex += up
-    while (coolString[index] == initValue or coolString[index] == 'w'
-           and iteration < math.floor(index)):
+    while ( index < len(coolString) and index > 0) and (coolString[index] == initValue or coolString[index] == 'w'):
         value += 1
         index += up
         iteration += 1
+ 
     return value
 
 
 maxe = 0
 coolString = s * 3
 initValue = coolString[index]
-
 for ite in range(index, 2 * index):
+    print(ite)
     lVal = findValues(1, ite)
     rVal = findValues(-1, ite)
     cVal = lVal + rVal -2
     if (cVal > maxe):
         maxe = cVal
 
-f = open("demofile2.txt", "w")
+f = open("beads.out", "w")
 
 if (maxe > index):
-    f.write(index+"\n")
+    f.write(str(index)+"\n")
     f.close()
 else:
-    f.write(maxe+"\n")
+    f.write(str(maxe)+"\n")
     f.close()
