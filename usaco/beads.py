47005b5bd199b3d8c89d48cdeade580879ffd361
Eddie Zhang <54998055+ezhang7423@users.noreply.github.com>
2019-12-08 19:28:20 -0800

    translate into py

--- usaco/beads.py
+++ usaco/beads.py
@@ -1,2 +1,36 @@
+import math
 data = open('beads.in', 'r')
-print(data.read().split('\n'))
+index, s = data.read().split('\n')
+
+
+def findValues(up, index):
+    value = 1
+    iteration = 0
+    initValue = coolString[index]
+    tIndex = index
+    while (initValue == 'w'):
+        initValue = coolString[tIndex + up]
+        tIndex += up
+    while (coolString[index + up] == initValue or coolString[index + up] == 'w'
+           and iteration < math.floor(index / 2)):
+        value += 1
+        index += up
+        iteration += 1
+    return value
+
+
+maxe = 0
+coolString = s * 3
+initValue = s[index]
+
+for ite in range(index, 2 * index):
+    lVal = findValues(1, ite)
+    rVal = findValues(-1, ite)
+    cVal = lVal + rVal
+    if (cVal > maxe):
+        maxe = cVal
+
+if (max > index):
+  print(index)
+else:
+  print(maxe)
