d34105006be6fe5bf5149ae561359dc30ec340f3
ezhang7423 <ezhang7423@student.palomar.edu>
2019-12-18 15:05:34 -0800

    start func

--- usaco/algorithms/binarysort.cpp
+++ usaco/algorithms/binarysort.cpp
@@ -3,14 +3,22 @@
 using namespace std;
 
 
+
+int* binarysort(int unsorted[], int low, int high){
+    int middle = high - low / 2;
+    
+}
+
 int main(int argc, char* argv[]){
     if (argc == 1){
         cout << "USAGE: ./binarysort 1 3 5 6 7...." << endl;
         exit(1);
     }
-    int unsorted[argc-1];
-    for (int i = 0; i < argc - 1; i++){
+    int len = argc -1;
+    int unsorted[len];
+    for (int i = 0; i < len; i++){
         unsorted[i] = atoi(argv[i+1]);
-        cout << unsorted[i] << " ";
     }
+    int* sorted = binarysort(unsorted, 0, len);
+
 }
