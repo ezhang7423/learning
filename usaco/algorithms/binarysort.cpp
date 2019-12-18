ac1d901309130dbc5c8b639cb5690cb239ef6883
ezhang7423 <ezhang7423@student.palomar.edu>
2019-12-18 15:20:21 -0800

    cpp sux

--- usaco/algorithms/binarysort.cpp
+++ usaco/algorithms/binarysort.cpp
@@ -5,10 +5,28 @@ using namespace std;
 
 
 int* binarysort(int unsorted[], int low, int high){
+    if (low >= high){
+        int* ans = &unsorted[low];
+        return ans;
+    }
     int middle = high - low / 2;
-    
+    int* firstHalf = binarysort(unsorted, 0, middle);
+    int* secondHalf = binarysort(unsorted, middle + 1, high);
+    int final[high-low];
+    for (int i = 0; i < middle; i++){
+        final[i] = firstHalf[i];
+    }
+    for (int i = middle + 1; i < high; i++){
+        final[i] = secondHalf[middle + 1 -i];
+    }
+    return final;
 }
 
+void print(int sorted[]){
+    for (int i = 0; i < *(&sorted + 1) - sorted; i++){
+        cout << sorted[i] << ' ';
+    }
+}
 int main(int argc, char* argv[]){
     if (argc == 1){
         cout << "USAGE: ./binarysort 1 3 5 6 7...." << endl;
@@ -20,5 +38,6 @@ int main(int argc, char* argv[]){
         unsorted[i] = atoi(argv[i+1]);
     }
     int* sorted = binarysort(unsorted, 0, len);
+    print(sorted);
 
 }
