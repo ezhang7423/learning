6b7d75b0a508e376093eca5c9cc320be765d6bee
ezhang7423 <ezhang7423@student.palomar.edu>
2019-12-21 12:12:42 -0800

    half

--- usaco/section1/namenum.py
+++ usaco/section1/namenum.py
@@ -1,10 +1,10 @@
 """
-ID: your_id_here
+ID: futzipe1
 LANG: PYTHON3
-TASK: test
+TASK: namenum
 """
 import math
-with open("test.out", 'w') as out, open("test.in", 'r', encoding='utf-8') as fin:
+with open("namenum.out", 'w') as out, open("namenum.in", 'r', encoding='utf-8') as fin:
     lines = fin.readlines()[0]
     lines = list(lines)
     mapp = {2: ["A", "B", "C"], 5: ["J", "K", "L"], 8: ["T", "U", "V"], 3: ["D", "E", "F"], 6: [
@@ -12,12 +12,13 @@ with open("test.out", 'w') as out, open("test.in", 'r', encoding='utf-8') as fin
     for x in range(len(lines)):
         lines[x] = int(lines[x])
     allP = []
-    for x in range(len(lines)-1):
-    for y in range(math.pow(3, x):
-        for z in range(math.pow(3, len(lines)-1)):
-            print('x:', x, 'y:', y, 'z:', y)
-            # currentIndex = y*math.pow(3, len(lines)-x) + z
-            # allP[currentIndex] = mapp[x][y]
+    for x in range(len(lines)-1):  # current number
+        for y in range(int(math.pow(3, x))+2):  # current character
+            for z in range(int(math.pow(3, len(lines)-x-1))):  # current index
+                print('x:', x, 'y:', y, 'z:', z)
+                currentIndex = y * math.pow(3, len(lines)-x-1) + z
+                print(currentIndex)
+                # allP[currentIndex] += mapp[x][y]
 
 
 """
@@ -31,3 +32,4 @@ for x in range(len(lines)-1):
         for z in range(math.pow(3, len(lines)-1)):
             currentIndex = y*math.pow(3, len(lines)-x) + z
             allP[currentIndex] = mapp[x][y]
+"""
