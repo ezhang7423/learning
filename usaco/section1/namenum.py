da68ad51ffb2bfb6eb91ffeca77318c55cffa7ec
Eddie Zhang <54998055+ezhang7423@users.noreply.github.com>
2019-12-21 13:29:11 -0800

    add numdict

--- usaco/section1/namenum.py
+++ usaco/section1/namenum.py
@@ -4,21 +4,31 @@ LANG: PYTHON3
 TASK: namenum
 """
 import math
+
+
+def toWord(wordMap, number):
+    number = paddNumber(number, len(lines))
+    number = list(number)
+    ans = ""
+    for x in range(len(number)):
+        ans += wordMap[x][number[x]]
+    return ans
+
+
 with open("namenum.out", 'w') as out, open("namenum.in", 'r', encoding='utf-8') as fin:
     lines = fin.readlines()[0]
     lines = list(lines)
     mapp = {2: ["A", "B", "C"], 5: ["J", "K", "L"], 8: ["T", "U", "V"], 3: ["D", "E", "F"], 6: [
         "M", "N", "O"], 9: ["W", "X", "Y"], 4: ["G", "H", "I"], 7: ["P", "R", 'S']}
+    wordMap = []
     for x in range(len(lines)):
         lines[x] = int(lines[x])
+        wordMap.append(mapp[lines[x]])
     allP = []
-    for x in range(len(lines)-1):  # current number
-        for y in range(int(math.pow(3, x))+2):  # current character
-            for z in range(int(math.pow(3, len(lines)-x-1))):  # current index
-                print('x:', x, 'y:', y, 'z:', z)
-                currentIndex = y * math.pow(3, len(lines) - x - 1) + z
-                print(currentIndex)
-                # allP[currentIndex] += mapp[x][y]
+
+    allP = set(allP)
+
+    out.write(str(union(allP, allW)))
 
 
 """
