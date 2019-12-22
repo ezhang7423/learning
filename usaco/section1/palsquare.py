050f18e2dd120d8c952189bd4979a1a4662dbed9
ezhang7423 <ezhang7423@student.palomar.edu>
2019-12-22 15:58:16 -0800

    done with two funcs

--- usaco/section1/palsquare.py
+++ usaco/section1/palsquare.py
@@ -3,24 +3,38 @@ ID: futzipe1
 LANG: PYTHON3
 TASK: palsquare
 """
+import math
 
 
 def toBase(base, num):
-    pass
+    ans = 0
+    while(num > 0):
+        nP = 1
+        index = 0
+        while nP * base <= num:
+            nP *= base
+            index += 1
+        vP = math.floor(num/nP)
+        ans += 10 ** (index) * vP
+        num -= vP * base ** index
+    return ans
 
 
 def isP(num):
-    pass
+    if (len(num) <= 1):
+        return True
+    if (num[0] != num[-1:]):
+        return False
+    return isP(num[1: -1])
 
-
-with open("palsquare.out", 'w') as out, open("palsquare.in", 'r', encoding='utf-8') as fin:
-    lines = fin.read().splitlines()  # lines is list type
-    base = int(lines[0])
-    ans = []
-    for x in range(300):
-        b10x = x ** 2
-        tBx = toBase(base, b10x)
-        if (isP(tBx)):
-            ans.append(str(x) + ' ' + str(b10x) + '\n')
-    for x in ans:
-        out.write(x)
+# with open("palsquare.out", 'w') as out, open("palsquare.in", 'r', encoding='utf-8') as fin:
+#     lines = fin.read().splitlines()  # lines is list type
+#     base = int(lines[0])
+#     ans = []
+#     for x in range(300):
+#         b10x = x ** 2
+#         tBx = toBase(base, b10x)
+#         if (isP(tBx)):
+#             ans.append(str(x) + ' ' + str(b10x) + '\n')
+#     for x in ans:
+#         out.write(x)
