71c38458faef2e2b12263034e878273ed500b39b
ezhang7423 <ezhang7423@student.palomar.edu>
2019-12-22 15:59:15 -0800

    chg index

--- usaco/section1/palsquare.py
+++ usaco/section1/palsquare.py
@@ -27,14 +27,15 @@ def isP(num):
         return False
     return isP(num[1: -1])
 
-# with open("palsquare.out", 'w') as out, open("palsquare.in", 'r', encoding='utf-8') as fin:
-#     lines = fin.read().splitlines()  # lines is list type
-#     base = int(lines[0])
-#     ans = []
-#     for x in range(300):
-#         b10x = x ** 2
-#         tBx = toBase(base, b10x)
-#         if (isP(tBx)):
-#             ans.append(str(x) + ' ' + str(b10x) + '\n')
-#     for x in ans:
-#         out.write(x)
+
+with open("palsquare.out", 'w') as out, open("palsquare.in", 'r', encoding='utf-8') as fin:
+    lines = fin.read().splitlines()  # lines is list type
+    base = int(lines[0])
+    ans = []
+    for x in range(1, 301):
+        b10x = x ** 2
+        tBx = toBase(base, b10x)
+        if (isP(str(tBx))):
+            ans.append(str(x) + ' ' + str(b10x) + '\n')
+    for x in ans:
+        out.write(x)
