d86947a82e8b33d5f3bccfae7f78f5d76deeb9ad
ezhang7423 <ezhang7423@student.palomar.edu>
2019-12-22 15:42:00 -0800

    basic logic

--- usaco/section1/palsquare.py
+++ usaco/section1/palsquare.py
@@ -3,6 +3,24 @@ ID: futzipe1
 LANG: PYTHON3
 TASK: palsquare
 """
+
+
+def toBase(base, num):
+    pass
+
+
+def isP(num):
+    pass
+
+
 with open("palsquare.out", 'w') as out, open("palsquare.in", 'r', encoding='utf-8') as fin:
     lines = fin.read().splitlines()  # lines is list type
-    out.write(str(lines))
+    base = int(lines[0])
+    ans = []
+    for x in range(300):
+        b10x = x ** 2
+        tBx = toBase(base, b10x)
+        if (isP(tBx)):
+            ans.append(str(x) + ' ' + str(b10x) + '\n')
+    for x in ans:
+        out.write(x)
