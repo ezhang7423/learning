4dfca5d3dd7aaa4b42972ba382107ddc2427bf4c
ezhang7423 <ezhang7423@student.palomar.edu>
2020-01-03 22:03:17 -0800

    ll

--- usaco/dataS/linkedlist.cpp
+++ usaco/dataS/linkedlist.cpp
@@ -3,10 +3,30 @@
 using namespace std;
 class intLinked{
     public:
-        void test(){
-            cout << "hello " << endl;
+        intLinked(){
+            head = 0;
+            tail = 0;
+        }
+        intLinked(int data){
+            Node* first = new Node{data, 0};
+            head = first;
+            tail = first;
+        }
+        void push(int data){
+            Node* temp = new Node{data, head};
+            head = temp;
+        }
+        void push_back(int data){
+            Node* temp = new Node{data, 0};
+            tail -> next = temp;
+            tail = temp;    
         }
     private:
+        struct Node{
+            int data;
+            Node* next;
+        };
+        Node* head, tail;
 };
 
 int main(){
