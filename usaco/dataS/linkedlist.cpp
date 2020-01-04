32f6b28ca5ae6e6f878ef94dc7a020910f70d6b9
ezhang7423 <ezhang7423@student.palomar.edu>
2020-01-03 21:57:08 -0800

    ok

---
+++ usaco/dataS/linkedlist.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+class intLinked{
+    public:
+        void test(){
+            cout << "hello " << endl;
+        }
+    private:
+};
+
+int main(){
+    intLinked th;
+    th.test();
+}
