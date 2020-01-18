07d82f2f1cbdb3bb15fd1ae0f47a86b8c6b97127
ezhang7423 <ezhang7423@student.palomar.edu>
2020-01-18 11:48:27 -0800

    i'm kinda confused not gonna lie

---
+++ usaco/w20bze/photo.py
@@ -0,0 +1,34 @@
+def gimmeAns(liste):
+    ans = []
+    for x in range(1, liste[0]):
+        current = x
+        pot = [current]
+        nut = current
+        for y in range(len(liste)):
+            nut = liste[y] - nut
+            if y == len(liste) - 1:
+                pot.append(nut)
+                ans.append(pot)
+                break
+            if nut >= liste[y+1]:
+                break
+
+            else:
+                pot.append(nut)
+    return ans
+
+
+def fmtAns(liste):
+    ans = ''
+    for x in range(len(liste)):
+        ans += str(liste[x])
+        if (x != len(liste) - 1):
+            ans += ' '
+    return ans
+
+
+with open('photo.in', 'r') as fin, open('photo.out', 'w') as fout:
+    inpute = fin.readlines()[1].split(' ')
+    data = [int(x) for x in inpute]
+    ans = gimmeAns(data)
+    fout.write(fmtAns(ans[len(ans)-1]))
