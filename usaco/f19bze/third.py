49ab8af38e84f0dfd5e2183d41d18c269ebc0b87
ezhang7423 <ezhang7423@student.palomar.edu>
2020-01-29 21:41:29 -0800

    finished

--- usaco/f19bze/third.py
+++ usaco/f19bze/third.py
@@ -16,6 +16,7 @@ for x in range(8):
 fin = open("lineup.in", 'r')
 data = fin.read().split("\n")[1:]
 fin.close()
+
 swaps = []
 for x in range(len(data)):
     swaps.append([nameNum[data[x].split(' ')[0]],
@@ -27,44 +28,39 @@ for x in range(len(swaps)):
     else:
         swaps2.append(swaps[x])
 swaps = sorted(list(set(tuple(i) for i in swaps2)))
-swaps2 = [swap for swap in swaps]
 print(swaps)
 
-chains = []
-marked = [0 for i in swaps]
-for x in range(len(swaps) - 1):
-    chained = False
-    if marked[x]:
-        continue
-    for y in range(x + 1, len(swaps)):
-        if swaps[x][0] == swaps[y][0]:
-            chained = True
-            e1 = swaps[x][1]
-            e2 = swaps[y][1]
-            marked[y] = 1
-            swaps2.remove(swaps[y])
-            swaps2.remove(swaps[x])
-            if (e1 > e2):
-                chains.append((e2, swaps[x][0], e1))
-            else:
-                chains.append((e1, swaps[x][0], e2))
-print(swaps2)
-print(chains)
-unique = []
-for x in range(len(swaps2)):
-    appended = False
-    for y in range(len(chains)):
-        if swaps2[x][0] == chains[y][1]:
-            chains[y] = chains[y] + (swaps2[x][1], )
-            appended = True
-        elif swaps2[x][1] == chains[y][0]:
-            chains[y] = (swaps2[x][0], ) + chains[y]
-            appended = True
-    if not appended:
-        unique.append(swaps2[x])
+
+def all_perms(elements):
+    if len(elements) <= 1:
+        yield elements
+    else:
+        for perm in all_perms(elements[1:]):
+            for i in range(len(elements)):
+                # nb elements[0:1] works in both string and list contexts
+                yield perm[:i] + elements[0:1] + perm[i:]
 
 
-ans =
-for x in range(len(unique)):
-    chains.append(unique[x])
-print(chains)
+potential = []
+for pot in all_perms([0, 1, 2, 3, 4, 5, 6, 7]):
+    pote = 0
+    for x in range(7):
+        for y in range(len(swaps)):
+            if (y == len(swaps) - 1):
+                if (pot[x] == swaps[y][0] and pot[x+1] == swaps[y][1] and pote == len(swaps) - 1):
+                    potential.append(pot)
+                elif (pot[x] == swaps[y][1] and pot[x+1] == swaps[y][0] and pote == len(swaps) - 1):
+                    potential.append(pot)
+            elif (pot[x] == swaps[y][0] and pot[x+1] == swaps[y][1]):
+                pote += 1
+            elif (pot[x] == swaps[y][1] and pot[x+1] == swaps[y][0]):
+                pote += 1
+
+
+ans = sorted(potential)[0]
+
+fout = open('lineup.out', 'w')
+for x in range(8):
+    print(numName[ans[x]])
+    fout.write(numName[ans[x]]+'\n')
+fout.close()
